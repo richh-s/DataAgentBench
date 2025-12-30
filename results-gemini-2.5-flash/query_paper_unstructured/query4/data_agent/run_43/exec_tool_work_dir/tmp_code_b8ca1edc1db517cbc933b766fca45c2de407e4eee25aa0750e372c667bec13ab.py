ada5d4f8edd64682ca8c8aa3736f4fe92a408bf8
Ruiying-Ma <ruiying021120@gmail.com>
2025-12-30 14:03:32 -0800

    add civic & paper for gemini-2.5-flash

---
+++ results-gemini-2.5-flash/query_paper_unstructured/query4/data_agent/run_43/exec_tool_work_dir/tmp_code_b8ca1edc1db517cbc933b766fca45c2de407e4eee25aa0750e372c667bec13ab.py
@@ -0,0 +1,5 @@
+code = """print('__RESULT__:\n"Simple test string"')"""
+
+env_args = {'var_function-call-16161537513363771660': 'file_storage/function-call-16161537513363771660.json', 'var_function-call-12752153450719007890': 'file_storage/function-call-12752153450719007890.json'}
+
+exec(code, env_args)
